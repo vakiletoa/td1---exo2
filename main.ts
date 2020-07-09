24acbce89d8fad52174e8928df8c21d71ecf355e
vakiletoa <67943344+vakiletoa@users.noreply.github.com>
2020-07-09 16:26:16 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+for (let index = 0; index < 2; index++) {
+    basic.showString("Bonjour")
+    basic.pause(500)
+    basic.showNumber(10853)
+    basic.pause(500)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+}
+basic.forever(function () {
+	
+})
